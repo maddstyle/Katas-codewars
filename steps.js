7c90778966488a598ab4bf3b6ab020e5cd639cbc
Michaela DeCamara <michaela.decamara@gmail.com>
2020-05-15 02:08:29 -0400

    capitalize and steps

---
+++ steps.js
@@ -0,0 +1,22 @@
+//iterative solution 
+function steps(n) {
+  for (let row = 0; row < n; row++) {
+    let stair = "";
+
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        stair += "#";
+      } else {
+        stair += " ";
+      }
+    }
+    console.log(stair);
+  }
+}
+
+module.exports = steps;
+
+//recursive solution 
+// function steps(n) {
+
+// }
