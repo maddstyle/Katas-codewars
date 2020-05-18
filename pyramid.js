5ed62b613c159cddd6a316eb1b5ef7ef79c7b702
Michaela DeCamara <michaela.decamara@gmail.com>
2020-05-17 23:58:46 -0400

    recursive version

--- pyramid.js
+++ pyramid.js
@@ -1,3 +1,4 @@
+//option 1 iterate version
 function pyramid(n) {
   const midpoint = Math.floor((2 * n - 1) / 2);
 
@@ -13,6 +14,29 @@ function pyramid(n) {
     }
     console.log(level)
   }
+}
+
+module.exports = pyramid;
+
+//option 2 recursive version
+function pyramid(n, row = 0, level = "") {
+  if (row === n) {
+    return;
+  }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  let add;
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = "#";
+  } else {
+    add = " ";
+  }
+  pyramid(n, row, level + add);
 }
 
 module.exports = pyramid;
