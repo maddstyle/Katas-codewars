55779915ebdb9e935051112fd0098c5a0a11f830
Michaela DeCamara <michaela.decamara@gmail.com>
2020-05-18 00:24:40 -0400

    vowels kata

---
+++ vowels.js
@@ -0,0 +1,24 @@
+//first solution
+function vowels(str) {
+  let count = 0;
+  const checker = ['a', 'e', 'i', 'o', 'u'];
+
+  for (let char of str.toLowerCase()) {
+    if (checker.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+module.exports = vowels;
+
+
+
+
+//second solution
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
+module.exports = vowels;
