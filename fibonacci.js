6e340906eda56cb9c3766251cab6f6f60f55dc80
Michaela DeCamara <michaela.decamara@gmail.com>
2020-05-21 23:50:02 -0400

    recursive solution to fibonacci kata

--- fibonacci.js
+++ fibonacci.js
@@ -22,3 +22,16 @@ function fib(n) {
 }
 
 module.exports = fib;
+
+
+//recursive solution
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+module.exports = fib;
+
