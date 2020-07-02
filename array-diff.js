536ea4fcc553ad3bf5254c3b6d8b33159adad693
maddstyle <michaela.decamara@gmail.com>
2020-07-01 23:30:31 -0400

    array-diff kata

---
+++ array-diff.js
@@ -0,0 +1,39 @@
+// our goal in this kata is to implement an difference function, which subtracts one list from another.
+
+// It should remove all values from list a, which are present in list b.
+
+// array_diff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+
+// array_diff([1,2,2,2,3],[2]) == [1,3]
+
+
+function array_diff(listA, listB) {
+
+  if (listB === undefined || listA === undefined) {
+    return listA;
+  }
+  
+  var diffArr = [];
+  var merged = listA.concat(listB);
+  
+  merged.forEach(function(el) {
+    if (!listB.includes(el)) {
+      diffArr.push(el);
+    }
+  });
+  
+  return diffArr;
+}
+
+//OR
+
+function array_diff(a, b) {
+  return a.filter(function(x) { return b.indexOf(x) == -1; });
+}
+
+function array_diff(a, b) {
+  return a.filter(function(x) { return !b.includes(x); });
+}
+
+array_diff([1,2,2,2,3],[2])
