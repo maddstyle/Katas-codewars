dbfbd079363d9152a26902c2ee5f447468c08d09
maddstyle <michaela.decamara@gmail.com>
2020-07-21 23:16:15 +0200

    ES6 version

--- Grasshopper-debug.js
+++ Grasshopper-debug.js
@@ -8,6 +8,21 @@
 // celsius = (fahrenheit - 32) * (5/9)
 // Remember that typically temperatures in the current weather conditions are given in whole numbers. It is possible for temperature sensors to report temperatures with a higher accuracy such as to the nearest tenth. Instrument error though makes this sort of accuracy unreliable for many types of temperature measuring sensors.
 
+function weatherInfo (temp) {
+  var c = convertToCelsius(temp);
+  if (c <= 0)
+    return (c + " is freezing temperature");
+  else
+    return (c + " is above freezing temperature");
+}
+
+function convertToCelsius (temperature) {
+  var celsius = (temperature - 32) * (5/9);
+  return celsius;
+}
+
+//ES6 version
+
 const weatherInfo = temperature => temperatureMesage(convertToCelsius(temperature));
 
 const convertToCelsius = temperature => (temperature - 32) * (5 / 9);
