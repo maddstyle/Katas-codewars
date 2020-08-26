1fd95a1c7fa2cea63ae766d02c76fde0dd8d4774
maddstyle <michaela.decamara@gmail.com>
2020-08-26 00:41:04 -0400

    node method .setNextNode()

--- basicNode.js
+++ basicNode.js
@@ -7,16 +7,30 @@
 
 // Underneath the node we previously created, use console.log() to print out the instance’s next node property. Check that null is output in the terminal.
 
+// 
+
+// In this exercise we need to implement .setNextNode() method in the Node class, so our next node can be updated from initial null value and used in more complex data structures. 
+
+// 1. implement .setNextNode on Node class. it should take node as an argument and update property next to an appropriate value.Node
+// 2. to verify that our funcition setNextNode is performing properly we need to call it on our Node instance. 
+// Create a second instance and store it in variable secondNode, then link it to the firstNode by passing it to the call .setNextNode().
+
+// console.log the firstNode to see it completed.
+
 class Node {
   constructor(data) {
     this.data = data;
     this.next = null;
   }
+  setNextNode(node) {
+    this.next = node;
+  }
 }
 
-const firstNode = new Node("I am the first instance.");
-console.log(firstNode.data);
-console.log(firstNode.next);
+const firstNode = new Node('I am the first instance.');
+const secondNode = new Node('I am the second instance');
 
+firstNode.setNextNode(secondNode);
+console.log(firstNode);
 
-module.export = Node;
+module.exports = Node;
