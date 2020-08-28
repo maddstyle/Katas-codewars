ea33347cf74b2d67b524f315e4ac77b3c48b6bf1
maddstyle <michaela.decamara@gmail.com>
2020-08-27 23:37:47 -0400

    tirangle and duck duck again

---
+++ loopingTriangle.js
@@ -0,0 +1,12 @@
+// Write a loop that makes seven calls to console.log to output the follorwing triangle
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+// ########
+
+for (let hashtag = "#"; hashtag.length < 8; hashtag += "#")
+console.log(hashtag);
