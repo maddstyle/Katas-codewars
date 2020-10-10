f9041238d8ac2394e158c822c682cdfbf1706fc2
maddstyle <michaela.decamara@gmail.com>
2020-10-09 23:40:42 -0400

    other versions of palidrome

--- palindrome.js
+++ palindrome.js
@@ -1,3 +1,15 @@
+// Return true if the given string is a palindrome. Otherwise, return false.
+
+// A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+
+// Note
+// You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case) in order to check for palindromes.
+
+// We'll pass strings with varying formats, such as "racecar", "RaceCar", and "race CAR" among others.
+
+// We'll also pass strings with special symbols, such as "2A3*3a2", "2A3 3a2", and "2_A3*3#A2".
+
+
 function palindrome(str) {
   str.split('').every((char, i) => {
      return char === str[str.length - i - 1];
@@ -5,3 +17,27 @@ function palindrome(str) {
 }
 
 module.exports = palindrome;
+
+// modern js
+function palindrome(str) {
+  var re = /[\W_]/g;
+  var lowRegStr = str.toLowerCase().replace(re, '');
+  var reverseStr = lowRegStr.split('').reverse().join(''); 
+  return reverseStr === lowRegStr;
+}
+palindrome("A man, a plan, a canal. Panama");
+
+
+//with for loop
+function palindrome(str) {
+  var re = /[^A-Za-z0-9]/g;
+  str = str.toLowerCase().replace(re, '');
+  var len = str.length;
+  for (var i = 0; i < len/2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+        return false;
+    }
+  }
+  return true;
+ }
+ palindrome("A man, a plan, a canal. Panama");
